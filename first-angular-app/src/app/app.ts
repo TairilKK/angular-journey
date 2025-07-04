db6b54950e36d88e7c8a6c53c4c0d104bb2b54b4
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-04 18:58:36 +0200

    add: tasks component

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { DUMMY_USERS } from './dummy-users';
 import { Header } from './header/header';
+import { Tasks } from './tasks/tasks';
 import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectUser(id: string) {
-    console.log('Selected user with id: ' + id);
+    this.selectedUserId = id;
   }
 }
