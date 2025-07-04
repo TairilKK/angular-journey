a0a1c720ae5d65039fabda4c6fd2d93a7932d2bf
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-04 18:30:51 +0200

    remove: commented code

--- first-angular-app/src/app/user/user.ts
+++ first-angular-app/src/app/user/user.ts
@@ -7,16 +7,10 @@ import { Component, computed, input } from '@angular/core';
   styleUrl: './user.css',
 })
 export class User {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
   avatar = input.required<string>();
   name = input.required<string>();
 
   imagePath = computed(() => 'assets/users/' + this.avatar());
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
 
   onSelectUser() {}
 }
