9659b769d57ce2b2761df0d947e21450bfee2a98
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-04 15:31:35 +0200

    add: user component to app

--- first-angular-app/src/app/app.ts
+++ first-angular-app/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
