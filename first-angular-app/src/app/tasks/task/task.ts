d9c211048f64e2f6db56dea19a44092ed610a178
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-07 18:06:46 +0200

    change: date formating with angular pipes

--- first-angular-app/src/app/tasks/task/task.ts
+++ first-angular-app/src/app/tasks/task/task.ts
@@ -1,10 +1,11 @@
+import { DatePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Card } from '../../shared/card/card';
 import { type TTask } from './task.model';
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
