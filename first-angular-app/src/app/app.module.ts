a3e47b98f44b5988b1fc1984f8dcf3dfc038a4b5
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-08 16:02:02 +0200

    add: shared module

--- first-angular-app/src/app/app.module.ts
+++ first-angular-app/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { App } from './app';
 import { Header } from './header/header';
-import { Card } from './shared/card/card';
+import { SharedModule } from './shared/shared.module';
 import { NewTask } from './tasks/new-task/new-task';
 import { Task } from './tasks/task/task';
 import { Tasks } from './tasks/tasks';
 import { User } from './user/user';
 
 @NgModule({
-  declarations: [App, Header, Task, Tasks, NewTask, User, Card],
+  declarations: [App, Header, Task, Tasks, NewTask, User],
   bootstrap: [App],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
