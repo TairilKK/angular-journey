3eee6149e296e5bb2a7f4044645672fb159b18a3
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-16 10:55:01 +0200

    add: data binding to form

--- investment-calculator-app/src/app/user-input/user-input.component.ts
+++ investment-calculator-app/src/app/user-input/user-input.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css',
 })
-export class UserInputComponent {}
+export class UserInputComponent {
+  initialInvestment = 0;
+  annualInvestment = 0;
+  expectedReturn = 5;
+  duration = 10;
+
+  onSubmit() {
+    console.log('calculate');
+  }
+}
