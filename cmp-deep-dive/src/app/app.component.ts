4c06bb04efa2ddba82ac6fdbe3862de4ed9b86ff
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-07-31 09:28:43 +0200

    remove: redundance

--- cmp-deep-dive/src/app/app.component.ts
+++ cmp-deep-dive/src/app/app.component.ts
@@ -3,10 +3,11 @@ import {HeaderComponent} from "./header/header.component";
 import {TrafficComponent} from "./dashboard/traffic/traffic.component";
 import {TicketsComponent} from "./dashboard/tickets/tickets.component";
 import {ServerStatusComponent} from "./dashboard/server-status/server-status.component";
+import {DashboardItemComponent} from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, TrafficComponent, TicketsComponent, ServerStatusComponent],
+  imports: [HeaderComponent, TrafficComponent, TicketsComponent, ServerStatusComponent, DashboardItemComponent],
   standalone: true,
   templateUrl: './app.component.html',
 })
