154c78b701d7f33735798a1c2350ef4b4771114e
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-05 14:21:29 +0200

    change: viewchild via signal

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, viewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from '../../../shared/button/button.component';
 import { ControlComponent } from '../../../shared/control/control.component';
@@ -11,12 +11,14 @@ import { ControlComponent } from '../../../shared/control/control.component';
   styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  form = viewChild<ElementRef<HTMLFormElement>>('form');
 
   onSubmit(titleInput: string, textInput: string) {
     console.log('Title:', titleInput);
     console.log('Text:', textInput);
 
-    this.form!.nativeElement.reset();
+    // this.form!.nativeElement.reset();
+    this.form()!.nativeElement.reset();
   }
 }
