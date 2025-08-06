8a7519a366e573780a4a6733956914cbd7a68fd9
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-06 13:54:08 +0200

    change: to two way binding

--- cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -24,6 +24,9 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
   // @Output() add = new EventEmitter<{title: string; text: string}>();
   add = output<{ title: string; text: string }>();
 
+  enteredTitle = '';
+  enteredText = '';
+
   ngOnInit(): void {
     console.log('NgOnInit');
     console.log('Form Element:', this.form()!.nativeElement);
@@ -34,8 +37,10 @@ export class NewTicketComponent implements OnInit, AfterViewInit {
     console.log('Form Element:', this.form()!.nativeElement);
   }
 
-  onSubmit(titleInput: string, textInput: string) {
-    this.add.emit({ title: titleInput, text: textInput });
-    this.form()!.nativeElement.reset();
+  onSubmit() {
+    this.add.emit({ title: this.enteredTitle, text: this.enteredText });
+    // this.form()!.nativeElement.reset();
+    this.enteredTitle = '';
+    this.enteredText = '';
   }
 }
