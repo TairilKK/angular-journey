e75ebe307e64472a8dbb753aea9f9d0d7f6224e0
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-06 13:24:23 +0200

    add: showing tickets

--- cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
+++ cmp-deep-dive/src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { TicketComponent } from './ticket/ticket.component';
 import { Ticket } from './tickets.model';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
