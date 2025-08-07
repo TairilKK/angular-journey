5ad4793248e2b5cadb7db2f577fdd15e92796aec
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-07 14:23:45 +0200

    add: custom property with directives

--- directives-deep-dive/src/app/safe-link.directive.ts
+++ directives-deep-dive/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from '@angular/core';
+import { Directive, input } from '@angular/core';
 
 @Directive({
   selector: 'a[appSafeLink]',
@@ -8,6 +8,8 @@ import { Directive } from '@angular/core';
   },
 })
 export class SafeLinkDirective {
+  queryParam = input<string>('myapp');
+
   constructor() {
     console.log('SafeLinkDirective is active!');
   }
@@ -17,7 +19,8 @@ export class SafeLinkDirective {
 
     if (wantsToLeave) {
       const address = (event.target as HTMLAnchorElement).href;
-      (event.target as HTMLAnchorElement).href = address + '?from=myapp';
+      (event.target as HTMLAnchorElement).href =
+        address + '?from=' + this.queryParam();
       return;
     }
 
