9090ea5fefba273e10c86a0befee713d0cc7d986
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-07 14:49:51 +0200

    fix: better way of adding directive in multiple place

--- directives-deep-dive/src/app/app.component.ts
+++ directives-deep-dive/src/app/app.component.ts
@@ -4,19 +4,13 @@ import { AuthComponent } from './auth/auth.component';
 import { AuthDirective } from './auth/auth.directive';
 import { AuthService } from './auth/auth.service';
 import { LearningResourcesComponent } from './learning-resources/learning-resources.component';
-import { LogDirective } from './log.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [
-    AuthComponent,
-    LearningResourcesComponent,
-    AuthDirective,
-    LogDirective,
-  ],
+  imports: [AuthComponent, LearningResourcesComponent, AuthDirective],
 })
 export class AppComponent {
   private authService = inject(AuthService);
