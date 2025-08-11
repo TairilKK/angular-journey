3b74e736a58ce12843c1cfb4373ba99cce68f79c
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-11 11:20:16 +0200

    add: custom observable

--- rxjs-deep-dive/src/app/app.component.ts
+++ rxjs-deep-dive/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { interval, map } from 'rxjs';
+import { interval, map, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +16,20 @@ export class AppComponent implements OnInit {
     initialValue: 0,
   });
 
+  customInterval$ = new Observable((subscriber) => {
+    let timesExecuted = 0;
+    const interval = setInterval(() => {
+      if (timesExecuted > 3) {
+        clearInterval(interval);
+        subscriber.complete();
+        return;
+      }
+      console.log('Emitting new value');
+      subscriber.next({ message: 'New value' });
+      timesExecuted++;
+    }, 2000);
+  });
+
   private destroyRef = inject(DestroyRef);
 
   constructor() {}
@@ -27,6 +41,11 @@ export class AppComponent implements OnInit {
         next: (val) => console.log(val),
       });
 
+    const subscribtionFromObservable = this.customInterval$.subscribe({
+      next: (val) => console.log(val),
+      complete: () => console.log('Completed'),
+    });
+
     const subscribtionFromSignal = this.clickCount$.subscribe({
       next: (val) => `Clicked button ${this.clickCount()} times.`,
     });
@@ -34,6 +53,7 @@ export class AppComponent implements OnInit {
     this.destroyRef.onDestroy(() => {
       subscribtion.unsubscribe();
       subscribtionFromSignal.unsubscribe();
+      subscribtionFromObservable.unsubscribe();
     });
   }
 
