2ff5727d3a949ae95729bbf6ebbcd491ce9a6bc9
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-12 14:22:23 +0200

    add: remove user fav places

--- http/src/app/places/user-places/user-places.component.ts
+++ http/src/app/places/user-places/user-places.component.ts
@@ -1,5 +1,6 @@
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
+import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
 import { PlacesService } from '../places.service';
@@ -34,6 +35,15 @@ export class UserPlacesComponent implements OnInit {
 
     this.destroyRef.onDestroy(() => {
       httpClientSubscribtion.unsubscribe();
+    });
+  }
+  onSelectPlace(place: Place) {
+    const onSelectionSubscribtion = this.placesService
+      .removeUserPlace(place)
+      .subscribe();
+
+    this.destroyRef.onDestroy(() => {
+      onSelectionSubscribtion.unsubscribe();
     });
   }
 }
