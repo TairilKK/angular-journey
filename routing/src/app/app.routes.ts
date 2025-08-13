e5a531e5212fea58f9153dd831352d2ff81bc8f4
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-13 18:17:39 +0200

    add: relative link patk

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { TaskComponent } from './tasks/task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
@@ -15,7 +15,7 @@ export const routes: Routes = [
     children: [
       {
         path: 'tasks',
-        component: TaskComponent,
+        component: TasksComponent,
       },
       {
         path: 'tasks/new',
--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,5 @@
 import { Component, computed, inject, input } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -7,7 +7,7 @@ import { UsersService } from '../users.service';
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent {
   userId = input.required<string>();
