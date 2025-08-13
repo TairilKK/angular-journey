ae1809773263b61d2175935bd2888f2bc9777f43
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-13 18:14:28 +0200

    add: nested routes

--- routing/src/app/app.routes.ts
+++ routing/src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { TaskComponent } from './tasks/task/task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [
@@ -10,5 +12,15 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks',
+        component: TaskComponent,
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+      },
+    ],
   },
 ];
--- routing/src/app/users/user-tasks/user-tasks.component.ts
+++ routing/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, computed, inject, input } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -6,6 +7,7 @@ import { UsersService } from '../users.service';
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet],
 })
 export class UserTasksComponent {
   userId = input.required<string>();
