5a9786082326bf79ecd45bc1d221f043e45abeaa
Jakub Dobrosielski <j.dobrosielski4@gmail.com>
2025-08-14 09:27:26 +0200

    add: query parameter and read query param

--- routing/src/app/tasks/tasks.component.ts
+++ routing/src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, computed, inject, input } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
 
@@ -8,10 +9,12 @@ import { TasksService } from './tasks.service';
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
+  order = input<'asc' | 'desc'>();
+
   private tasksService = inject(TasksService);
   userTasks = computed(() =>
     this.tasksService.allTasks().filter((task) => task.userId === this.userId())
